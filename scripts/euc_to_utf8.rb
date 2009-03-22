24a7cbf59917016178d5eadfb91c768ad430b124
kroenecker <mojowings@gmail.com>
2009-03-22 13:33:13 +0900

    Replaced uconv with iconv.

--- scripts/euc_to_utf8.rb
+++ scripts/euc_to_utf8.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/ruby -w
 # simple script for turning the euc file into unicode 8
-require 'uconv'
+require 'iconv'
 
 begin
   utf8file = File.open("edict_utf8.txt", "w")
   File.open("edict").each do |line|
-    utf8 = Uconv.euctou8(line)
-    utf8file << utf8
+    utf8file << Iconv.iconv("utf-8","euc-jp",line)
   end
   utf8file.close
 end
